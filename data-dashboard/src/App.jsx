7a5176cf0549848b11f815089f1dbf1f618a95f5
Laura Zhang <laura.zhang167@gmail.com>
2025-07-12 23:05:45 -0400

    created project files and api key

---
+++ data-dashboard/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
